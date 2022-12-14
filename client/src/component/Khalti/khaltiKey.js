58532432af81c755daf51b3ec38f18b80433062a
demon-ora <110587646+demon-ora@users.noreply.github.com>
2022-12-14 17:25:37 +0545

    Update khaltiKey.js

--- client/src/component/Khalti/khaltiKey.js
+++ client/src/component/Khalti/khaltiKey.js
@@ -1,6 +1,6 @@
 let myKey = {
-  publicTestKey:"test_public_key_0904fbfdf87b48668973b8f99f312f42",
-  secretKey:"test_secret_key_4caee9e36c66484497c2d3b53e9675d4",
+  publicTestKey:"put your public key",
+  secretKey:"put your secret key",
 };
 
 export default myKey;
